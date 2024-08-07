8cc4b9136644fa617f2ce722bd8aa51f35f2c037
hunmuk <ihm2119@naver.com>
2024-08-07 17:18:23 +0900

    등록페이지

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref(0)
+//const count = 0;
+</script>
+
+<template>
+  <br>
+  <div>
+    <input type="text" placeholder="제목을 입력해주세요"/>
+  </div>
+  <div>
+    <textarea rows="15" placeholder="내용을 입력해주세요"></textarea>
+  </div>
+  <button>작성</button>
+</template>
+
+<style scoped>
+
+</style>
